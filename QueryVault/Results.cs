c38007088ced1451af6e0c760f47333427a53425
Alex Fielder <alex.fielder@gmail.com>
2015-03-24 14:36:21 +0000

    created a better approach that allows for updating/searching from the same dataset.

---
+++ QueryVault/Results.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QueryVault
+{
+    public class Results
+    {
+        public string dwgnum { get; set; }
+        public string filename { get; set; }
+    }
+}
